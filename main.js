8f1d9976c4785f6588f38b1491f07a40eacbca96
nadiawaseem <161966596+Nadiawaseem@users.noreply.github.com>
2024-06-08 17:33:53 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function city_country(nameofcity, nameofcountry) {
+    return '${nameofcity} ${nameofcountry}';
+}
+var citya = city_country("Lahore", "Pakistan");
+var cityb = city_country("Dubai", "usa");
+var cityc = city_country("Colambo", "srilanka");
+var cityd = city_country("Dhaka", "Bangladesh");
+console.log(citya);
+console.log(cityb);
+console.log(cityc);
+console.log(cityd);
---
+++ main.ts
@@ -0,0 +1,11 @@
+  function city_country(nameofcity:string,nameofcountry:string){
+    return '${nameofcity} ${nameofcountry}'
+  }
+  let citya = city_country("Lahore","Pakistan")
+  let cityb = city_country("Dubai","usa")
+  let cityc = city_country("Colambo","srilanka")
+  let cityd = city_country("Dhaka","Bangladesh")
+  console.log(citya);
+  console.log(cityb);
+  console.log(cityc);
+  console.log(cityd);
